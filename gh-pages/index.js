f4e98c29781c596b53c9c4c3d8d65f82c17d1cbe
Blixt <blixt@spotify.com>
2014-03-10 17:23:06 -0400

    Add Ctrl+Enter support to demo

--- gh-pages/index.js
+++ gh-pages/index.js
@@ -7,7 +7,7 @@ function output(text, opt_class) {
   document.getElementById('output').appendChild(element);
 }
 
-document.getElementById('run').addEventListener('click', function () {
+function run() {
   var input = document.getElementById('input');
 
   output(input.value + '\n', 'code');
@@ -16,4 +16,14 @@ document.getElementById('run').addEventListener('click', function () {
   });
 
   input.value = '';
+}
+
+document.addEventListener('keydown', function (event) {
+  // Ctrl+Enter and Cmd+Enter is the same as hitting "Run code".
+  if (event.keyCode != 13 || (!event.ctrlKey && !event.metaKey)) return;
+
+  run();
+  event.preventDefault();
 });
+
+document.getElementById('run').addEventListener('click', run);
